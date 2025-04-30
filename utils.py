62a0589ef565c9c7cfe3374ab6462a8657370d9f
ZeBaoB <bill.njonze@gmail.com>
2025-04-30 11:51:03 +0200

    MAJ files

--- utils.py
+++ utils.py
@@ -238,7 +238,7 @@ def generate_Heston_scenarios(df_params : pd.DataFrame, dBdW : pd.DataFrame, beg
     sigma = df_params.loc['sigma']
     nb_stocks = len(mu)
     if V_init is None:
-        V_init = df_params.loc['V_end']
+        V_init = df_params.loc['theta']
     # generate dates excluding Saturdays and Sundays
     dates = pd.date_range(start=beginDate, end=endDate, freq='B')
     nb_periods = len(dates)
@@ -257,7 +257,7 @@ def generate_Heston_scenarios(df_params : pd.DataFrame, dBdW : pd.DataFrame, beg
         variance_path = np.zeros((nb_periods, nb_stocks))
         variance_path[0, :] = V_init.values
         for j in range(1, nb_periods):
-            variance_path[j, :] = np.maximum(variance_path[j-1, :] + kappa.values * (theta.values - variance_path[j-1, :]) * delta_t[j] + sigma.values * np.sqrt(variance_path[j-1, :]) * lReturns_Variance[j, nb_stocks:, i], 0) * delta_t[j]**0.5
+            variance_path[j, :] = np.maximum(variance_path[j-1, :] + kappa.values * (theta.values - variance_path[j-1, :]) * delta_t[j] + sigma.values * np.sqrt(variance_path[j-1, :]) * lReturns_Variance[j, nb_stocks:, i] * (delta_t[j]**0.5) , 0)
         variance_paths[f"Scenario {i+1}"] = pd.DataFrame(variance_path, index=dates, columns=df_params.columns)
     
     # Log return paths
